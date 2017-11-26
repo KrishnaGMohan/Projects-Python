ac213d7b3f43912905bb264af09c19bf3fb59684
Krishna Gandhi Mohan <krgandhi@microsoft.com>
2017-11-26 09:03:42 -0500

    Add docstring to list_moves

--- FunStuff/FunStuff/ktour.py
+++ FunStuff/FunStuff/ktour.py
@@ -31,6 +31,7 @@ def display_board(board):
     print("\t    " + " ----" * 8)
 
 def list_moves(board, pos):
+	""" Finds all possible moves on a board from a given position """
     rx = pos[0]
     cx = pos[1] 
     kr = [-2, -2, -1, -1,  1, 1,  2, 2]
